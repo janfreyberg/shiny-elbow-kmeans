284dcd2f4ab4bd487711ad08120ec3235936ecb6
Jan Freyberg <jan.freyberg@gmail.com>
2017-01-28 19:22:35 +0000

    Include output of results
    Also improved the reactivity

--- server.R
+++ server.R
@@ -10,81 +10,82 @@ library(stringr)
 library(ggplot2)
 library(magrittr)
 library(tibble)
+library(dplyr)
 
 shinyServer(function(input, output) {
   
-  output$elbowPlot <- renderPlot({
-    
-    # process the data
-    variable <- as.matrix(as.numeric(unlist(str_split(input$datatext, '(\n|,|[:space:])+'))))
-    
-    # perform the k-means analyses
-    krange <- 1:min(15, (nrow(variable)-1))
-    wss <- krange
-    for(i in krange){
-      set.seed(2017)
-      wss[i] <- sum(kmeans(variable, i, iter.max=20)$withinss)
-    }
-    
-    elbowdata <<- tibble(krange, wss)
-    
-    
-    # make a plot
-    p <- ggplot(elbowdata, aes(x=krange, y=wss)) +
-      geom_line() +
-      geom_point(shape = 1, size = 3, fill = 'white') +
-      # make it nice
-      theme_bw() +
-      scale_x_discrete(breaks=krange, labels=krange) +
-      labs(y = "Sum of squares explained",
-           x = "Number of clusters found")
-    
-    p
+  # Obtain the data
+  inputData <- reactive({
+    tibble(y = str_trim(input$datatext) %>%
+                str_split('(\n|,|[:space:])+') %>%
+                unlist() %>%
+                as.numeric() %>%
+                .[!is.na(.)])
   })
   
-  output$clusterPlot <- renderPlot({
-    # process input data
-    variable <- as.numeric(unlist(str_split(input$datatext, '(\n|,|[:space:])+')))
-
-    # find the selected k value
-    k <- nearPoints(elbowdata, input$plot_click, threshold = 20, maxpoints = 1)$krange
-    if(length(k) == 0){k = 1}
-    # perform the k-means analysis again
-    if(length(k) > 0){
-      
-      set.seed(2017) # to achieve same result as above
-      kmeansresult <- kmeans(as.matrix(variable), k, iter.max=20)
-      clustering <- kmeansresult$cluster
-      centers <- tibble(y = c(kmeansresult$centers), x = rep(1, k),
-                        group = as.factor(1:k))
-      
-    } else {
-      
-      clustering <- rep(1, length(variable))
-      
-    }
-    
-    clusterdata <- tibble(variable, group=as.factor(clustering), x=rep(1, length(variable)))
-    # create plot
-    set.seed(2017) # so that jitter doesn't get re-done
-    p <- ggplot(clusterdata, aes(x = x, y = variable,
-                                 color = group)) +
-      geom_point(size=4, position=position_jitter(width=0.15)) +
-      xlim(0, 2) +
-      scale_color_discrete() +
-      scale_x_discrete() +
+  # Do the cluster analysis (reruns only if input changes)
+  kclusts <- reactive({
+    set.seed(2017)
+    data.frame(k=1:min(15, nrow(inputData())-1)) %>%
+      group_by(k) %>%
+      do(kclust=kmeans(inputData()$y, .$k, iter.max=50))
+  })
+  
+  # Make the elbow plot
+  output$elbowPlot <- renderPlot({
+    # get the summary data out of the data frame
+    kclusts() %>%
+      group_by(k) %>%
+      do(glance(.$kclust[[1]])) %>%
+      # Do the plot:
+      ggplot(aes(x=k, y=tot.withinss)) +
+      geom_line() +
+      geom_point(shape = 1, size = 3, fill = 'white') +
+      # Make it nice
       theme_bw() +
-      theme(legend.position=c(0.9, 0.5))
-      labs(x="", y="Your Variable.")
-
-    # add the group centers to the plot (if existent)
-    if(length(k) > 0){
-      p <- p + geom_point(data=centers, aes(x = x, y = y, color = group),
-                          shape=10, size=7)
-    }
+      scale_x_discrete(breaks=krange, labels=krange) +
+      labs(y = "(Residual) Variation not explained",
+           x = "Number of clusters")
+  })
+  
+  # Get the selected k-point
+  selectedk <- reactive({
+    # obtain the click
+    interacted <- input$plot_click
+    # if click occurred and is not 0:
+    if(!is.null(interacted)){
+      min(max(round(interacted$x), 1),
+          nrow(inputData())-1)
+    }else{1}
+  })
+  
+  # Plot the raw data
+  output$clusterPlot <- renderPlot({
+    set.seed(2017) # so that the jitter isn't redone all the time
+    cat("whatever")
+    centers <- kclusts() %>% filter(k==selectedk()) %>% group_by(k) %>% do(tidy(.$kclust[[1]])) %>% mutate(y = x1, x = 1)
     
-    # display plot
-    p
+    kclusts() %>%
+      filter(k==selectedk()) %>% group_by(k) %>% do(augment(.$kclust[[1]], inputData())) %>% mutate(x = 1) %>%
+      ggplot(aes(x=x, y=y, colour=.cluster)) +
+        # Add the original points
+        geom_point(size=4, position=position_jitter(width=0.15)) +
+        # Add the cluster centers
+        geom_point(data=centers, aes(colour=cluster), size = 20, shape = '*', show_guide=FALSE) +
+        # Make it pretty
+        xlim(0.5, 2.5) + scale_color_discrete() + scale_x_discrete() + theme_bw() +
+        theme(legend.position=c(0.8, 0.5)) + labs(x="", y="Your Variable.", color="Cluster")
+  })
+  
+  output$kmeansResults <- renderText({
+    
+    kclust <- kclusts() %>% filter(k==selectedk()) %>% .$kclust %>% .[[1]]
+    centers <- kclust %$% toString(round(centers, digits=2))
+    variance <- round(100*(kclust$totss-kclust$tot.withinss)/kclust$totss, digits=2)
+    
+    paste0("You chose ", selectedk(), ifelse(selectedk()>1, " clusters.", " cluster."), "\n\n",
+           ifelse(selectedk()>1, "The cluster centers are:\n", "The cluster center (i.e. overall mean) is:\n"), centers, "\n\n",
+           "This clustering accounts for ", variance, "% of the total variance.")
   })
 
 })
