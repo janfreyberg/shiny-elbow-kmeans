c8d9c0e05824a8f282e77ab08c774df3977b2085
janfreyberg <jan.freyberg@gmail.com>
2017-02-01 15:07:02 +0000

    Change the bivariate data
    Using 30 points from the xclara data set from R package 'cluster'

--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ shinyUI(fluidPage(
 
   # Application title
   titlePanel("Bivariate k-Means Clustering with elbow method"),
-  helpText("Find the code for this at http://github.com/janfreyberg/shiny-elbow-kmeans.",
+  helpText("Find the code for this at https://github.com/janfreyberg/shiny-elbow-kmeans/tree/bivariate.",
            "Find the accompanying blogpost at <missing>"),
   hr(),
   # Sidebar with a slider input for number of bins
@@ -23,12 +23,12 @@ shinyUI(fluidPage(
              column(6, textAreaInput(
               "datatext1",
               "Paste Data (Variable 1) (comma/tab/space separated)",
-              value = "1, 2, 3, 60, 70, 80, 100, 220, 230, 250"
+              value = "31.38396, 17.07317, 13.47672, -1.164893, 57.22761, 48.80508, 50.47091, 11.65338, 91.93916, 16.6546, -3.438403, 70.81757, 4.773241, 42.125, 14.02917, 67.88265, 28.51895, 42.79715, 5.145525, -0.7703359, 18.1468, 9.10579, 68.26376, 72.77863, 18.98427, 8.106246, 9.05127, 82.44264, 28.05616, 4.492934"
             )),
              column(6, textAreaInput(
                "datatext2",
                "Paste Data (Variable 2) (comma/tab/space separated)",
-               value = "4, 3, 2, 60, 2, 3, 2, 210, 215, 212"
+               value = "66.525, 25.84198, -4.123874, 18.86306, -5.167954, -22.4844, 64.8867, -5.657587, -1.150087, -1.519889, -12.02527, -20.52531, 11.55061, 66.37105, 2.41937, 60.98403, 73.79701, 51.57061, 11.186, 24.88074, 1.836594, 19.51155, -19.15556, 1.591326, 70.79776, 0.4920085, 12.58227, -17.38876, 31.56779, 16.61764"
             ))
            ),
            h3("Step 2: Find the elbow"),
